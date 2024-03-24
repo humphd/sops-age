ce936d3e19fcf144f0a32f632defbaf6375e683c
David Humphrey <david.andrew.humphrey@gmail.com>
2024-03-24 18:45:17 -0400

    Initial setup

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,11 @@
 export interface GreetOptions {
-	logger?: (message: string) => void;
-	message: string;
-	times?: number;
+  logger?: (message: string) => void;
+  message: string;
+  times?: number;
+}
+
+export interface SopsJson extends Record<string, any> {
+  sops: {
+    age: { enc: string; recipient: string }[];
+  };
 }
